61f9aaef89a9df9605256a75c5a67ca866f96ff5
Haroith <mitasov.home@gmail.com>
2024-06-03 01:55:19 +0500

    ErlangB: main program.
    -(add) - added functions;
    -(add) - added main program.

--- erlangB.php
+++ erlangB.php
@@ -13,17 +13,27 @@ $seviceLevelGoal = 80; // 80%
 // We need to know more statistics
 $aht = 63; // average handling time in seconds
 
+// Prepare the data
+$ErlangB = new ErlangB($callsForecast, $aht);
+
+// Now we have to 'guess' number of agents
+$calculatedSL = 0;
+$agents = 0;
+while ($calculatedSL > (1-$seviceLevelGoal/100)) {
+    $agents += 1;
+    $calculatedSL = $ErlangB->ProbabilityOfBlocking($agents);
+}
+print_r($agents);
+print_r('/n');
+print_r($calculatedSL);
 
 class ErlangB {
 
-    public function __construct()
+    private $erlangs;
+
+    public function __construct($calls, $averageHandlingTime)
     {
-
-    }
-
-    private function ProbabilityOfBlocking($erlangs, $agents) {
-        $probabilityOfBlocking = (pow($erlangs, $agents) / gmp_fact($agents)) / (1);
-        return $probabilityOfBlocking;
+        $this->erlangs = $this->Erlangs($calls, $averageHandlingTime);
     }
 
     private function Erlangs($calls, $averageHandlingTime) {
@@ -31,9 +41,17 @@ class ErlangB {
         return $erlangs;
     }
 
-    private function DenominatorForPb() {
-        $denominator = 1;
+    private function DenominatorForPb($agents) {
+        $denominator = 0;
+        for ($i = 0; $i <= $agents; $i++){
+            $denominator += pow($this->erlangs, $i)/gmp_fact($i);
+        }
         return $denominator;
+    }
 
+    public function ProbabilityOfBlocking($agents) {
+        $probabilityOfBlocking =
+        (pow($this->erlangs, $agents) / gmp_fact($agents)) / $this->DenominatorForPb($agents);
+        return $probabilityOfBlocking;
     }
 }
