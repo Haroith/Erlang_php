45a2f5a8a246fcc42c7b7287ed94a9a16aa1f97a
Haroith <mitasov.home@gmail.com>
2024-06-05 22:00:32 +0500

    ErlangC: main program.
    -(add) - added and fixed main program.

--- erlangC.php
+++ erlangC.php
@@ -8,6 +8,8 @@
 $callsForecast15min = 47; // 47 calls per 15 minutes
 $serviceLevelPercentsGoal = 80; // 80%
 $serviceLevelTimeGoal = 20; // 20 seconds
+// convert
+$serviceLevelTimeGoalMinutes = $serviceLevelTimeGoal / 60;
 
 // ErlangC formulae work with queue, so SL=80%/20seconds.
 
@@ -20,19 +22,25 @@ $ErlangC = new ErlangC($callsForecast15min, $averageHandlingTimeSeconds);
 // Formula is too complex, we cannot revert it
 // Now we have to 'guess' number of agents
 // Minimum number of agents = load from ErlangC class
-$agents = 7;
-$calculatedSL = $ErlangC->ServiceLevelPercents($agents, $serviceLevelTimeGoal);
+$calculatedSL = 1;
+$agents = ceil($ErlangC->a);
+while ($calculatedSL < $serviceLevelPercentsGoal) {
+    $agents++;
+    $calculatedSL = $ErlangC->ServiceLevelPercents($agents, $serviceLevelTimeGoalMinutes);
+}
+$calculatedASA = $ErlangC->AverageSpeedAnswerSeconds($agents);
 
 print_r('required agents='.$agents.'.');
 print_r("\n");
 print_r('resulted service level='.$calculatedSL.'%');
-
+print_r("\n");
+print_r('resulted average speed of answer='.$calculatedASA.' minutes');
 
 class ErlangC {
 
     private $lambda; // callsForecast per 1 minute
     private $beta; // averageHandlingTime in minutes
-    private $a; // load in erlangs
+    public $a; // load in erlangs
     public function __construct($callsForecast15min, $averageHandlingTimeSeconds)
     {
         $this->lambda = $callsForecast15min / 15;
@@ -56,23 +64,30 @@ class ErlangC {
     // $s = number of agents
     private function SumForDP($s) {
         $sumForDP = 0;
-        for ($j = 0; $j <= ($s-1); $j++){
-            $sumForDP += (pow($this->a, $j) / $this->factorial($j));
+        for ($j = 0; $j <= $s-1; $j++){
+            $sumForDP += pow($this->a, $j) / $this->factorial($j);
         }
         return $sumForDP;
     }
 
     // $s = number of agents
     private function DelayProbability($s) {
-        $delayProbability = (pow($this->a, $s)/($this->factorial($s-1)*($s-$this->a))) *
-            pow(($this->SumForDP($s)+(pow($this->a, $s)/($this->factorial($s-1)*($s-$this->a)))), -1);
+        $delayProbability = (pow($this->a, $s) / ($this->factorial($s-1)*($s-$this->a))) *
+            pow($this->SumForDP($s) + (pow($this->a, $s)/($this->factorial($s-1)*($s-$this->a))), -1);
         return $delayProbability;
     }
 
     // $s = number of agents
     // $t = service level goal time in seconds
     public function ServiceLevelPercents($s, $t) {
-        $serviceLevelPercents = 1 - $this->DelayProbability($s) * exp(-($s/$this->beta-$this->lambda)*$t);
+        $serviceLevelPercents = 1 - $this->DelayProbability($s) * exp(-1* ($s/$this->beta-$this->lambda) * $t);
+        $serviceLevelPercents *= 100;
         return $serviceLevelPercents;
+    }
+
+    // $s = number of agents
+    public function AverageSpeedAnswerSeconds($s) {
+        $averageSpeedAnswerSeconds = ($this->DelayProbability($s) * $this->beta) / ($s - $this->a);
+        return $averageSpeedAnswerSeconds;
     }
 }
